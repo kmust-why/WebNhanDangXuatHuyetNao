c5b7fe14baa9b83a0af5c580cb086464847f80ea
TranPhanAnTruong <Tr.Phan.Truong@gmail.com>
2018-05-20 01:51:37 +0700

    Cập nhật
    - Tách nhiều hình ra khi nhận dạng

--- Home/urls.py
+++ Home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.getViewTrangChu, name='getViewTrangChu'),
     path('upload-file', views.postUpload, name='postUpload'),
     path('doc-thong-tin-file-dicom', views.docThongTinFileDicom, name='docThongTinFileDicom'),
+    path('nhan-dang-vung-xuat-huyet-nao', views.nhanDangVungXuatHuyet, name='nhanDangVungXuatHuyet'),
     path('thu-vien', views.ThuVien, name='ThuVien'),
     path('tim-hieu-xuat-huyet-nao', views.ThuVien, name='ThuVien'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -51,12 +51,17 @@ def docThongTinFileDicom(request):
     else:
         return HttpResponse("FILE NOT FOUND")
 
-def NhanDang(duongDanHinh, tenFile):
+@csrf_exempt
+def nhanDangVungXuatHuyet(request):
+    tenFile = request.POST.get("tenFile")
     sys.path.append("..")
     MODEL_NAME = 'modules/inference_graph'
-    IMAGE_NAME = duongDanHinh
+    IMAGE_NAME = "static/uploads/images/" + tenFile + ".png"
+
+    print(IMAGE_NAME)
+
     CWD_PATH = os.getcwd()
-    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'15000_frozen_inference_graph.pb')
     PATH_TO_LABELS = os.path.join(CWD_PATH,'modules/training','labelmap.pbtxt')
     PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
     NUM_CLASSES = 4
@@ -64,6 +69,8 @@ def NhanDang(duongDanHinh, tenFile):
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
     category_index = label_map_util.create_category_index(categories)
     detection_graph = tf.Graph()
+
+    # Khai báo tri thức
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
         with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
@@ -71,24 +78,51 @@ def NhanDang(duongDanHinh, tenFile):
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
         sess = tf.Session(graph=detection_graph)
+
+    # Lấy dữ liệu từ file tri thức
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
     detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    #
     image = cv2.imread(PATH_TO_IMAGE)
     image_expanded = np.expand_dims(image, axis=0)
     (boxes, scores, classes, num) = sess.run(
         [detection_boxes, detection_scores, detection_classes, num_detections],
         feed_dict={image_tensor: image_expanded})
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        np.squeeze(scores),
-        category_index,
-        use_normalized_coordinates=True,
-        line_thickness=8,
-        min_score_thresh=0.80)
-    cv2.imwrite("static/uploads/images-detect/" + tenFile + ".png", image)
-    return "static/uploads/images-detect/" + tenFile + ".png"
+
+    arrayPath = []
+    indexName = 0
+
+    for indexRow, row in enumerate(scores):
+        for indexCol, item in enumerate(row):
+            if item >= 0.8:
+
+                arrayboxes = []
+                arrayclasses = []
+                arrayscores = []
+                
+                renewImg = cv2.imread(PATH_TO_IMAGE)
+
+                arrayboxes.append(boxes[indexRow][indexCol])
+                arrayclasses.append(classes[indexRow][indexCol])
+                arrayscores.append(scores[indexRow][indexCol])
+
+                print(arrayboxes)
+
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    renewImg,
+                    np.array(arrayboxes),
+                    np.array(arrayclasses).astype(np.int32),   
+                    np.array(arrayscores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8,
+                    min_score_thresh=0.80)
+                cv2.imwrite("static/uploads/images-detect/" + tenFile + "-" + str(indexName) + ".png", renewImg)
+                indexName += 1
+
+
+    return HttpResponse(tenFile)
